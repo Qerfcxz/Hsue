8505160bc59bb94c42883ee09feb1ac9b437f99e
Qerfcxz <1425430529@qq.com>
2026-05-28 00:23:14 +0800

    Add files via upload

--- src/Engine/Engine.hs
+++ src/Engine/Engine.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedRecordDot #-}
 
 module Engine.Engine where
 
@@ -12,7 +13,8 @@ import qualified SDL.Function as F
 import qualified Data.Int as DI
 import qualified Data.IntMap as DIM
 import qualified Data.Map as DM
-import qualified Data.Sequence as DS
+import qualified Data.Sequence as DSeq
+import qualified Data.Set as DSet
 import qualified Data.Word as DW
 import qualified Foreign.Marshal.Alloc as FMA
 import qualified Foreign.Ptr as FP
@@ -25,8 +27,8 @@ quit_engine=F.sdl_quit
 
 create_engine::Maybe DI.Int32->(Engine a->Event->Maybe Int)->a->Engine a
 create_engine timer main_id state=case timer of
-    Nothing->Engine {state=state,leaf=DIM.empty,node=DIM.empty,window=DIM.empty,window_id_map=DM.empty,request=DS.empty,main_id=main_id,timer=Keep_off}
-    Just time->Engine {state=state,leaf=DIM.empty,node=DIM.empty,window=DIM.empty,window_id_map=DM.empty,request=DS.empty,main_id=main_id,timer=Keep_on {time=fromIntegral time}}
+    Nothing->Engine {state=state,leaf=DIM.empty,node=DIM.empty,window=DIM.empty,window_map=DM.empty,request=DSeq.empty,key=DSet.empty,main_id=main_id,timer=Keep_off}
+    Just time->Engine {state=state,leaf=DIM.empty,node=DIM.empty,window=DIM.empty,window_map=DM.empty,request=DSeq.empty,key=DSet.empty,main_id=main_id,timer=Keep_on {time=fromIntegral time}}
 
 run_engine::Engine a->IO ()
 run_engine engine=FMA.allocaBytes C.sdl_event_size $ \ptr->case engine.timer of
@@ -39,10 +41,10 @@ run_engine engine=FMA.allocaBytes C.sdl_event_size $ \ptr->case engine.timer of
 loop_engine::FP.Ptr ()->Engine a->IO ()
 loop_engine ptr engine=do
     new_engine<-run_request engine
-    event<-get_event ptr
+    (event,key)<-get_event ptr engine.window_map engine.key
     case event of
         Quit->return ()
-        _->let new_new_engine=run_event event new_engine in case new_new_engine.timer of
+        _->let new_new_engine=run_event event (new_engine {key=key}) in case new_new_engine.timer of
             Keep_off->loop_engine ptr new_new_engine
             Turn_on {time}->do
                 now<-F.sdl_getticks
@@ -55,12 +57,12 @@ loop_engine_time time next_time ptr engine=do
     now<-F.sdl_getticks
     if now<next_time
         then do
-            event<-get_event_time ptr (fromIntegral (next_time-now))
+            (event,key)<-get_event_time (fromIntegral (next_time-now)) ptr engine.window_map engine.key
             case event of
                 Quit->return ()
-                Time->loop_engine_time_a (next_time+time) ptr (run_event Time new_engine)
-                _->loop_engine_time_a next_time ptr (run_event event new_engine)
-        else loop_engine_time_a (next_time+time) ptr (run_event Time new_engine)
+                Time->loop_engine_time_a (next_time+time) ptr (run_event Time (new_engine {key=key}))
+                _->loop_engine_time_a next_time ptr (run_event event (new_engine {key=key}))
+        else loop_engine_time_a (max (next_time+time) (now+time)) ptr (run_event Time new_engine)
 
 loop_engine_time_a::DW.Word64->FP.Ptr ()->Engine a->IO ()
 loop_engine_time_a next_time ptr engine=case engine.timer of
@@ -73,8 +75,8 @@ loop_engine_time_a next_time ptr engine=case engine.timer of
 
 run_request::Engine a->IO (Engine a)
 run_request engine=case engine.request of
-    DS.Empty->return engine
-    (request DS.:<| other_request)->do
+    DSeq.Empty->return engine
+    (request DSeq.:<| other_request)->do
         new_engine<-do_request request (engine {request=other_request})
         run_request new_engine
 
@@ -91,7 +93,7 @@ run_event_a leaf_id cache event engine=case DIM.lookup leaf_id engine.leaf of
         Just node_id->case DIM.lookup node_id cache of
             Nothing->let engine_node=engine.node in case DIM.lookup node_id engine_node of
                 Nothing->error "run_event_a: error 2"
-                Just node->let (seq_node_id,new_event)=run_event_c cache node.ancestry (DS.singleton node_id) event in let (new_cache,new_new_event)=run_event_d engine engine_node seq_node_id cache new_event in run_event_b leaf new_cache new_new_event event engine
+                Just node->let (seq_node_id,new_event)=run_event_c cache node.ancestry (DSeq.singleton node_id) event in let (new_cache,new_new_event)=run_event_d engine engine_node seq_node_id cache new_event in run_event_b leaf new_cache new_new_event event engine
             Just new_event->run_event_b leaf cache new_event event engine
 
 run_event_b::Leaf a->DIM.IntMap Event->Event->Event->Engine a->Engine a
@@ -99,15 +101,15 @@ run_event_b leaf cache new_event event engine=let new_engine=run_widget new_even
     Nothing->new_engine
     Just new_leaf_id->run_event_a new_leaf_id cache event new_engine
 
-run_event_c::DIM.IntMap Event->DS.Seq Int->DS.Seq Int->Event->(DS.Seq Int,Event)
-run_event_c _ DS.Empty seq_node_id event=(seq_node_id,event)
-run_event_c cache (other_node_id DS.:|> node_id) seq_node_id event=let maybe_event=DIM.lookup node_id cache in case maybe_event of
-    Nothing->run_event_c cache other_node_id (node_id DS.<| seq_node_id) event
+run_event_c::DIM.IntMap Event->DSeq.Seq Int->DSeq.Seq Int->Event->(DSeq.Seq Int,Event)
+run_event_c _ DSeq.Empty seq_node_id event=(seq_node_id,event)
+run_event_c cache (other_node_id DSeq.:|> node_id) seq_node_id event=let maybe_event=DIM.lookup node_id cache in case maybe_event of
+    Nothing->run_event_c cache other_node_id (node_id DSeq.<| seq_node_id) event
     Just new_event->(seq_node_id,new_event)
 
-run_event_d::Engine a->DIM.IntMap (Node a)->DS.Seq Int->DIM.IntMap Event->Event->(DIM.IntMap Event,Event)
-run_event_d _ _ DS.Empty cache event=(cache,event)
-run_event_d engine engine_node (node_id DS.:<| other_node_id) cache event=case DIM.lookup node_id engine_node of
+run_event_d::Engine a->DIM.IntMap (Node a)->DSeq.Seq Int->DIM.IntMap Event->Event->(DIM.IntMap Event,Event)
+run_event_d _ _ DSeq.Empty cache event=(cache,event)
+run_event_d engine engine_node (node_id DSeq.:<| other_node_id) cache event=case DIM.lookup node_id engine_node of
     Nothing->error "run_event_d: error 1"
     Just node->let new_event=node.event_transform engine event in run_event_d engine engine_node other_node_id (DIM.insert node_id new_event cache) new_event
 
--- src/Engine/Type.hs
+++ src/Engine/Type.hs
@@ -14,11 +14,11 @@ import qualified Data.Word as DW
 import qualified Foreign.C.Types as FCT
 import qualified Foreign.Ptr as FP
 
-data Engine a=Engine {state::a,leaf::DIM.IntMap (Leaf a),node::DIM.IntMap (Node a),window::DIM.IntMap Window,window_id_map::DM.Map DW.Word32 Int,request::DSeq.Seq (Request a),main_id::Engine a->Event->Maybe Int,timer::Timer}
+data Engine a=Engine {state::a,leaf::DIM.IntMap (Leaf a),node::DIM.IntMap (Node a),window::DIM.IntMap Window,window_map::DM.Map DW.Word32 Int,request::DSeq.Seq (Request a),key::DSet.Set Key,main_id::Engine a->Event->Maybe Int,timer::Timer}
 
 data Leaf a=Leaf {father::Maybe Int,next::Engine a->Event->Maybe Int,widget::Widget a}
 
-data Node a=Node {ancestry::DSeq.Seq Int,leaf_child::DIS.IntSet,node_child::DIS.IntSet,event_transform::Engine a->Event->Event,widget_transform::Engine a->Widget a->Widget a}
+data Node a=Node {ancestry::DSeq.Seq Int,leaf_child::DIS.IntSet,node_child::DIS.IntSet,event_transform::Engine a->Event->Event,widget_transform::Engine a->Request a->Widget a->Widget a}
 
 data Widget a=Trigger {trigger::Event->Engine a->Engine a}|Io_trigger {io_trigger::Event->Engine a->IO (Engine a)}
 
@@ -28,6 +28,12 @@ data Timer=Keep_off|Keep_on {time::DW.Word64}|Turn_off|Turn_on {time::DW.Word64}
 
 data Window=Window {window_id::Int,sdl_window::FP.Ptr T.SDL_window,sdl_renderer::FP.Ptr T.SDL_renderer,window_widget::DIS.IntSet}
 
-data Event=Unknown|Quit|Time
+data Event=Unknown|Quit|Time|At {window_id::Int,action::Action}
+
+data Action=Close|Press {press::Press,keycode::Key,set_keycode::DSet.Set Key}
+
+data Press=Press_up|Press_down
+
+data Key=Key_unknown|Key_a|Key_b|Key_c|Key_d|Key_e|Key_f|Key_g|Key_h|Key_i|Key_j|Key_k|Key_l|Key_m|Key_n|Key_o|Key_p|Key_q|Key_r|Key_s|Key_t|Key_u|Key_v|Key_w|Key_x|Key_y|Key_z deriving (Eq,Ord)
 
 data Window_flag=Window_resizable
