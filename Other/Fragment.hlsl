2dafc6aeb0178ba3e7676ddc2b76d0def5aed4f8
Qerfcxz <1425430529@qq.com>
2026-06-15 23:46:16 +0800

    Add files via upload

--- Other/Fragment.hlsl
+++ Other/Fragment.hlsl
@@ -1,10 +1,26 @@
 Texture2D texture:register(t0,space2);
 SamplerState this_sampler:register(s0,space2);
 struct VSOutput {
-    float4 color:COLOR;
+    float4 color:TEXCOORD0;
     float4 xy:SV_Position;
-    float2 uv:TEXCOORD0;
+    float2 uv:TEXCOORD1;
+    float4 clip:TEXCOORD2;
+    float size:TEXCOORD3;
+    float2 screen:TEXCOORD4;
+    float scale:TEXCOORD5;
 };
 float4 main(VSOutput output):SV_Target {
-    return output.color*texture.Sample(this_sampler,output.uv);
+    float out_of=(output.screen.x<output.clip.x||output.screen.y<output.clip.y||output.screen.x>output.clip.z||output.screen.y>output.clip.w)?0:1;
+    float4 texture_color=texture.Sample(this_sampler,output.uv);
+    float4 final_color;
+    if (output.size>0) {
+        float sd=max(min(texture_color.r,texture_color.g),min(max(texture_color.r,texture_color.g),texture_color.b))-0.5;
+        final_color=float4(output.color.rgb,output.color.a*clamp(sd*output.scale+0.5,0,1));
+    } else {
+        final_color=output.color*texture_color;
+    }
+    if (out_of<1) {
+        discard;
+    }
+    return final_color;
 }
--- Other/Vertex.hlsl
+++ Other/Vertex.hlsl
@@ -1,21 +1,47 @@
 cbuffer Window:register(b0,space1) {
     float2 size;
-    float2 padding;
+    float font_size;
+    float pixel_range;
 }
 struct VSInput {
     float4 color:TEXCOORD0;
     float2 xy:TEXCOORD1;
     float2 uv:TEXCOORD2;
+    float parameter_id:TEXCOORD3;
+    float size:TEXCOORD4;
 };
 struct VSOutput {
-    float4 color:COLOR;
+    float4 color:TEXCOORD0;
     float4 xy:SV_Position;
-    float2 uv:TEXCOORD0;
+    float2 uv:TEXCOORD1;
+    float4 clip:TEXCOORD2;
+    float size:TEXCOORD3;
+    float2 screen:TEXCOORD4;
+    float scale:TEXCOORD5;
 };
+struct Parameter {
+    float2 xy;
+    float2 x;
+    float2 y;
+    float2 parameter_padding;
+    float4 clip;
+};
+StructuredBuffer<Parameter> parameter_buffer:register(t0,space0);
+float2 apply_matrix(float2 matrix_xy,float2 matrix_x,float2 matrix_y,float2 xy) {
+    float2 new_xy=xy-matrix_xy;
+    return matrix_xy+float2(matrix_x.x*new_xy.x+matrix_x.y*new_xy.y,matrix_y.x*new_xy.x+matrix_y.y*new_xy.y);
+}
 VSOutput main(VSInput input) {
     VSOutput output;
     output.color=input.color;
-    output.xy=float4(2*input.xy.x/size.x,2*input.xy.y/size.y,0,1);
+    Parameter parameter=parameter_buffer[(uint)input.parameter_id];
+    float2 new_input=apply_matrix(parameter.xy,parameter.x,parameter.y,input.xy);
+    output.xy=float4(2*new_input.x/size.x,2*new_input.y/size.y,0,1);
     output.uv=input.uv;
+    output.clip=parameter.clip;
+    output.size=input.size;
+    output.screen=input.xy;
+    float matrix_scale=length(parameter.x);
+    output.scale=(4*pixel_range*input.size)/font_size;
     return output;
 }
